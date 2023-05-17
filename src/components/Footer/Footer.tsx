86d14704cacba79b0929f36a23bf08a404a4c4f7
Maj Knez <actuallydoc@gmail.com>
2023-05-17 20:21:00 +0200

    vercel fix

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <div className="site-divider"></div>
+            <div className="flex justify-center p-4">
+                <h1 className="text-white text-2xl font-bold">Made with ❤️ by actuallydoc</h1>
+            </div>
+        </div>
+
+    )
+}
