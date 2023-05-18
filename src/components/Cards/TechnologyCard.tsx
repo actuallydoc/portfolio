addee543d046aa1a3e71c4327528d232901e6f03
Maj Knez <actuallydoc@gmail.com>
2023-05-18 20:03:36 +0200

    Not responsive

--- src/components/Cards/TechnologyCard.tsx
+++ src/components/Cards/TechnologyCard.tsx
@@ -109,7 +109,7 @@ export default function TechnologyCard() {
             {/* Make responsible because website is glitching and it goes outside the webpage on the right side */}
             <div className='flex flex-col items-center justify-center mt-5 p-5 space-y-5 bg-gradient-to-b from-[#1e1629] to-[#04061d] rounded-lg shadow-2xl'>
                 <h4 className='text-2xl text-center text-slate-400'>Technologies i have tried</h4>
-                <div className='sm:flex-col md:grid grid-cols-8 p-2'>
+                <div className='sm:flex-col md:grid grid-cols-8 p-2 overflow-auto'>
                     {KnownTechnologies.map((tech, index) => (
                         <div key={index} className='flex items-center mx-1 my-1 justify-center w-16 h-16 bg-gray-800 rounded-full'>
                             <Image src={tech.image} className='rounded-full' alt='' width={50} height={50} />
@@ -121,7 +121,7 @@ export default function TechnologyCard() {
                     <div>
                         <h4 className='text-xl text-center text-slate-400'>Technologies i want to learn in the future!</h4>
                     </div>
-                    <div className='sm:flex-col md:grid grid-cols-7 space-x-1'>
+                    <div className='sm:flex-col md:grid grid-cols-7 space-x-1 '>
                         {FutureTechnologies.map((tech, index) => (
                             <div key={index} className='flex items-center justify-center mx-1 my-1 w-16 h-16 bg-gray-800 rounded-full'>
                                 <Image src={tech.image} className='rounded-full' alt='' width={50} height={50} />
